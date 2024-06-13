b520f63aaa45ad223b3802f8afc8778f88b79015
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-06-13 12:13:17 +0700

    implementasi insert

--- BinarySearchtree/BinarySearchtree.cpp
+++ BinarySearchtree/BinarySearchtree.cpp
@@ -20,4 +20,11 @@ public:
 
     BinaryTree() {
         ROOT = nullptr; //initializing ROOT to null
+
+        void insert(string element) { // insert a node in the binary search tree
+            Node* newNode = new Node(element, nullptr, nullptr); // allocate memory for the new node
+            newNode->info = element; //assign value to the data field of the new node
+            newNode->leftchild = nullptr; //make the left child of the new node point to null
+            newNode->rightchild = nullptr; // make the right child of the new node point to NULL
+
     }
